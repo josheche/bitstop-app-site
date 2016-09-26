fb487cdb33533464a3c596f8ffb1aa320dd23264
Joshua Echeverri <josheche@gmail.com>
2016-09-26 13:20:11 -0400

    bitstop screenshot iphone

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -40,14 +40,14 @@ $(window).scroll( function(){
 
 //Code for smooth scrolling:
 $(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
       if (target.length) {
-        $('html,body').animate({
+        $('html, body').animate({
           scrollTop: target.offset().top
-        }, 1000);
+        }, 1500);
         return false;
       }
     }
