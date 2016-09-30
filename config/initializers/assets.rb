859cc14cb0fbeb29200a73bb4720c6ac9f8da8d0
Joshua Echeverri <josheche@gmail.com>
2016-09-30 16:21:44 -0400

    config assets issue

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,4 +8,6 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
- Rails.application.config.assets.precompile += %w( assets/* )
+ Rails.application.config.assets.precompile += %w( assets/images/* )
+ Rails.application.config.assets.precompile += %w( assets/stylesheets/* )
+ Rails.application.config.assets.precompile += %w( assets/javascripts/* )
