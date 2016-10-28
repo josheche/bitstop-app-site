a976a2743fe3cbefd382d8905b70b933a1489a19
Joshua Echeverri <josheche@gmail.com>
2016-10-28 13:10:28 -0400

    app store links and smartbanner js stuffs meta tags galore and heroku pushes work again

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   get "/bitstop" => redirect("https://bitstop.co")
 
+  get "/android" => redirect("https://play.google.com/store/apps/details?id=co.bitstop.android")
+
+  get "/apple" => redirect("https://itunes.apple.com/us/app/bitcoin-tools/id1163764342?mt=8")
+
   root to: 'visitors#index'
 end
